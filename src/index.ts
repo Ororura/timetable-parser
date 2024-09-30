68650ca7fb07427243df5468b1a950e6d1907d1c
Ororura <hitmanlol890@gmail.com>
2024-09-30 11:51:50 +0300

    change message logic

--- src/index.ts
+++ src/index.ts
@@ -17,23 +17,47 @@ let selectedGroup: string | undefined = '';
 bot.onText(/\/start/, async (msg) => {
 	const chatId = msg.chat.id;
 
-	bot.sendMessage(
-		chatId,
-		'Привет, я расскажу тебе пары на сегодня. Введи номер дня\n1. Понедельник\n2. Вторник\n3. Среда\n4. Четверг\n5. Пятница\n6. Суббота\n7. Воскресенье',
-		{
-			reply_markup: {
-				keyboard: [
-					[{ text: '/день 1' }],
-					[{ text: '/день 2' }],
-					[{ text: '/день 3' }],
-					[{ text: '/день 4' }],
-					[{ text: '/день 5' }],
-					[{ text: '/день 6' }],
-					[{ text: '/день 7' }],
-				],
+	await bot.sendMessage(chatId, 'Привет, я расскажу тебе пары на сегодня. Введи номер группы');
+
+	bot.once('message', async (msg) => {
+		const groupId = msg.text;
+
+		const data = await readGroups(
+			'https://e-spo.ru/org/rasp/export/site/index?pid=1&RaspBaseSearch%5Bgroup_id%5D=&RaspBaseSearch%5Bsemestr%5D=osen&RaspBaseSearch%5Bprepod_id%5D=',
+		);
+
+		if (!groupId) {
+			await bot.sendMessage(chatId, 'Группа не найдена');
+			return;
+		}
+
+		selectedGroup = data.get(groupId);
+
+		if (!selectedGroup) {
+			await bot.sendMessage(chatId, 'Группа не найдена');
+			return;
+		}
+
+		await bot.sendMessage(chatId, `Выбрана группа ${groupId}`);
+
+		await bot.sendMessage(
+			chatId,
+			'Теперь выбери день\n1. Понедельник\n2. Вторник\n3. Среда\n4. Четверг\n5. Пятница\n6. Суббота\n7. Воскресенье',
+			{
+				reply_markup: {
+					keyboard: [
+						[{ text: '/день 1' }],
+						[{ text: '/день 2' }],
+						[{ text: '/день 3' }],
+						[{ text: '/день 4' }],
+						[{ text: '/день 5' }],
+						[{ text: '/день 6' }],
+						[{ text: '/день 7' }],
+					],
+				},
 			},
-		},
-	);
+		);
+	});
 });
 
 bot.onText(/\/день (.+)/, async (msg, match) => {
@@ -43,15 +67,9 @@ bot.onText(/\/день (.+)/, async (msg, match) => {
 	}
 
 	const chatId = msg.chat.id;
-
 	const number = parseInt(match[1]);
 
-	if (isNaN(number)) {
-		await bot.sendMessage(chatId, 'Введи корректный номер дня (от 1 до 7)');
-		return;
-	}
-
-	if (!number || number < 1 || number > 7) {
+	if (isNaN(number) || number < 1 || number > 7) {
 		await bot.sendMessage(chatId, 'Введи корректный номер дня (от 1 до 7)');
 		return;
 	}
@@ -69,27 +87,4 @@ bot.onText(/\/день (.+)/, async (msg, match) => {
 	} else {
 		await bot.sendMessage(chatId, 'Нет данных для выбранного дня');
 	}
-});
-
-bot.onText(/\/группа (.+)/, async (msg, match) => {
-	const chatId = msg.chat.id;
-	
-	if (!match) {
-		await bot.sendMessage(msg.chat.id, 'Введи корректный номер дня (от 1 до 7)');
-		return;
-	}
-
-	const groupId = match[1];
-	const data = await readGroups(
-		'https://e-spo.ru/org/rasp/export/site/index?pid=1&RaspBaseSearch%5Bgroup_id%5D=&RaspBaseSearch%5Bsemestr%5D=osen&RaspBaseSearch%5Bprepod_id%5D=',
-	);
-
-	selectedGroup = data.get(groupId);
-
-	if (!selectedGroup) {
-		await bot.sendMessage(chatId, 'Группа не найдена');
-		return;
-	}
-
-	await bot.sendMessage(chatId, `Выбрана группа ${groupId}`);
 });
