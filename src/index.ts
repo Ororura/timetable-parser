945f0a68e23fd6036bb02eb11295917189232b74
Ororura <hitmanlol890@gmail.com>
2024-09-30 13:36:34 +0300

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -20,29 +20,29 @@ bot.onText(/\/start/, async (msg) => {
 	await bot.sendMessage(chatId, 'Привет, я расскажу тебе пары на сегодня. Введи номер группы');
 
 	bot.once('message', async (msg) => {
-		const groupId = msg.text;
-
+		const groupName = msg.text;
+		
+		if (!groupName) {
+			await bot.sendMessage(chatId, 'Группа не найдена');
+			return;
+		}
+		
 		const data = await readGroups(
 			'https://e-spo.ru/org/rasp/export/site/index?pid=1&RaspBaseSearch%5Bgroup_id%5D=&RaspBaseSearch%5Bsemestr%5D=osen&RaspBaseSearch%5Bprepod_id%5D=',
 		);
 
-		if (!groupId) {
-			await bot.sendMessage(chatId, 'Группа не найдена');
-			return;
-		}
-
-		selectedGroup = data.get(groupId);
+		selectedGroup = data.get(groupName);
 
 		if (!selectedGroup) {
 			await bot.sendMessage(chatId, 'Группа не найдена');
 			return;
 		}
 
-		await bot.sendMessage(chatId, `Выбрана группа ${groupId}`);
+		await bot.sendMessage(chatId, `Выбрана группа ${groupName}`);
 
 		await bot.sendMessage(
 			chatId,
-			'Теперь выбери день\n1. Понедельник\n2. Вторник\n3. Среда\n4. Четверг\n5. Пятница\n6. Суббота\n7. Воскресенье',
+			'Теперь выбери день\n1. Понедельник\n2. Вторник\n3. Среда\n4. Четверг\n5. Пятница\n6. Суббота',
 			{
 				reply_markup: {
 					keyboard: [
@@ -52,7 +52,6 @@ bot.onText(/\/start/, async (msg) => {
 						[{ text: '/день 4' }],
 						[{ text: '/день 5' }],
 						[{ text: '/день 6' }],
-						[{ text: '/день 7' }],
 					],
 				},
 			},
@@ -61,6 +60,11 @@ bot.onText(/\/start/, async (msg) => {
 });
 
 bot.onText(/\/день (.+)/, async (msg, match) => {
+	if (selectedGroup === '') {
+		await bot.sendMessage(msg.chat.id, 'Выбери группу');
+		return;
+	}
+
 	if (!match) {
 		await bot.sendMessage(msg.chat.id, 'Введи корректный номер дня (от 1 до 7)');
 		return;
@@ -80,7 +84,12 @@ bot.onText(/\/день (.+)/, async (msg, match) => {
 
 	if (response && response[number] && typeof response[number] === 'object' && 'lessons' in response[number]) {
 		const lessons = response[number].lessons
-			.map((lesson, index) => `#${index + 1}\n${lesson.time}\n${lesson.teacher}\n${lesson.discipline}`)
+			.map((lesson, index) => {
+				if (!lesson.teacher) {
+					return `#${index + 1} \n----------------------------------------`;
+				}
+				return `#${index + 1}\n${lesson.time}\nКабинет: ${lesson.room}\n${lesson.teacher}\n${lesson.discipline}`;
+			})
 			.join('\n\n');
 
 		await bot.sendMessage(chatId, lessons);
