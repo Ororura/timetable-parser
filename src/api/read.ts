744142df125fd81a76888f560ef5cc365964dfe1
Ororura <hitmanlol890@gmail.com>
2024-10-07 09:29:25 +0300

    bug fix

--- src/api/read.ts
+++ src/api/read.ts
@@ -1,36 +1,35 @@
 import { parseGroups, parseHtml } from '../services/parse.js';
 
 const readLessons = async (url: string) => {
-	const response = await fetch(url);
+  const response = await fetch(url);
 
-	if (!response.ok) {
-		throw new Error(`HTTP error! Status: ${response.status}`);
-	}
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
 
-	const contentType = response.headers.get('content-type');
-	if (contentType && contentType.includes('text/html')) {
-		const text = await response.text();
-		return parseHtml(text);
-	} else {
-		throw new Error(`Expected HTML but received ${contentType}`);
-	}
+  const contentType = response.headers.get('content-type');
+  if (contentType && contentType.includes('text/html')) {
+    const text = await response.text();
+    return parseHtml(text);
+  } else {
+    throw new Error(`Expected HTML but received ${contentType}`);
+  }
 };
 
 const readGroups = async (url: string) => {
-	const response = await fetch(url);
+  const response = await fetch(url);
 
-	if (!response.ok) {
-		throw new Error(`HTTP error! Status: ${response.status}`);
-	}
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
 
-	const contentType = response.headers.get('content-type');
-	if (contentType && contentType.includes('text/html')) {
-		const text = await response.text();
-		return parseGroups(text);
-	} else {
-		throw new Error(`Expected HTML but received ${contentType}`);
-	}
+  const contentType = response.headers.get('content-type');
+  if (contentType && contentType.includes('text/html')) {
+    const text = await response.text();
+    return parseGroups(text);
+  } else {
+    throw new Error(`Expected HTML but received ${contentType}`);
+  }
 };
-
 
 export { readLessons, readGroups };
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import TelegramBot from 'node-telegram-bot-api';
 import dotenv from 'dotenv';
 
 type Command = {
-	command: string;
-	description: string;
+  command: string;
+  description: string;
 };
 
 dotenv.config();
@@ -12,12 +12,12 @@ dotenv.config();
 const token = process.env.API_KEY;
 
 const commands: Command[] = [
-	{ command: '/start', description: 'Начало работы' },
-	{ command: '/day', description: 'Выбрать день' },
+  { command: '/start', description: 'Начало работы' },
+  { command: '/day', description: 'Выбрать день' },
 ];
 
 if (!token) {
-	throw new Error('Токен не найден');
+  throw new Error('Токен не найден');
 }
 
 const bot = new TelegramBot(token, { polling: true });
@@ -25,89 +25,89 @@ const bot = new TelegramBot(token, { polling: true });
 let selectedGroup: string | null = null;
 
 bot.onText(/\/start/, async (msg) => {
-	const chatId = msg.chat.id;
+  const chatId = msg.chat.id;
 
-	await bot.sendMessage(chatId, 'Привет, я расскажу тебе пары на сегодня. Введи номер группы');
+  await bot.sendMessage(chatId, 'Привет, я расскажу тебе пары на сегодня. Введи номер группы');
 
-	bot.once('message', async (msg) => {
-		const groupName = msg.text;
+  bot.once('message', async (msg) => {
+    const groupName = msg.text;
 
-		if (!groupName) {
-			await bot.sendMessage(chatId, 'Группа не найдена');
-			return;
-		}
+    if (!groupName) {
+      await bot.sendMessage(chatId, 'Группа не найдена');
+      return;
+    }
 
-		const data = await readGroups(
-			'https://e-spo.ru/org/rasp/export/site/index?pid=1&RaspBaseSearch%5Bgroup_id%5D=&RaspBaseSearch%5Bsemestr%5D=osen&RaspBaseSearch%5Bprepod_id%5D=',
-		);
+    const data = await readGroups(
+      'https://e-spo.ru/org/rasp/export/site/index?pid=1&RaspBaseSearch%5Bgroup_id%5D=&RaspBaseSearch%5Bsemestr%5D=osen&RaspBaseSearch%5Bprepod_id%5D=',
+    );
 
-		const group = data.get(groupName);
+    const group = data.get(groupName);
 
-		if (!group) {
-			await bot.sendMessage(chatId, 'Группа не найдена');
-			return;
-		}
+    if (!group) {
+      await bot.sendMessage(chatId, 'Группа не найдена');
+      return;
+    }
 
-		selectedGroup = group;
+    selectedGroup = group;
 
-		await bot.sendMessage(chatId, `Выбрана группа ${groupName}`);
+    await bot.sendMessage(chatId, `Выбрана группа ${groupName}`);
 
-		await bot.sendMessage(
-			chatId,
-			'Теперь выбери день\n1. Понедельник\n2. Вторник\n3. Среда\n4. Четверг\n5. Пятница\n6. Суббота',
-			{
-				reply_markup: {
-					keyboard: [
-						[{ text: '/day 1' }],
-						[{ text: '/day 2' }],
-						[{ text: '/day 3' }],
-						[{ text: '/day 4' }],
-						[{ text: '/day 5' }],
-						[{ text: '/day 6' }],
-					],
-				},
-			},
-		);
-	});
+    await bot.sendMessage(
+      chatId,
+      'Теперь выбери день\n1. Понедельник\n2. Вторник\n3. Среда\n4. Четверг\n5. Пятница\n6. Суббота',
+      {
+        reply_markup: {
+          keyboard: [
+            [{ text: '/day 1' }],
+            [{ text: '/day 2' }],
+            [{ text: '/day 3' }],
+            [{ text: '/day 4' }],
+            [{ text: '/day 5' }],
+            [{ text: '/day 6' }],
+          ],
+        },
+      },
+    );
+  });
 });
 
 bot.onText(/\/day (.+)/, async (msg, match) => {
-	if (selectedGroup === null) {
-		await bot.sendMessage(msg.chat.id, 'Выбери группу');
-		return;
-	}
+  if (selectedGroup === null) {
+    await bot.sendMessage(msg.chat.id, 'Выбери группу');
+    return;
+  }
 
-	if (!match) {
-		await bot.sendMessage(msg.chat.id, 'Введи корректный номер дня (от 1 до 7)');
-		return;
-	}
+  if (!match) {
+    await bot.sendMessage(msg.chat.id, 'Введи корректный номер дня (от 1 до 7)');
+    return;
+  }
 
-	const chatId = msg.chat.id;
-	const number = parseInt(match[1]);
+  const chatId = msg.chat.id;
+  const number = parseInt(match[1]);
 
-	if (isNaN(number) || number < 1 || number > 7) {
-		await bot.sendMessage(chatId, 'Введи корректный номер дня (от 1 до 7)');
-		return;
-	}
+  if (isNaN(number) || number < 1 || number > 7) {
+    await bot.sendMessage(chatId, 'Введи корректный номер дня (от 1 до 7)');
+    return;
+  }
 
-	const response = await readLessons(
-		`https://e-spo.ru/org/rasp/export/site/index?pid=1&RaspBaseSearch%5Bgroup_id%5D=${selectedGroup}&RaspBaseSearch%5Bsemestr%5D=osen&RaspBaseSearch%5Bprepod_id%5D=`,
-	);
+  const response = await readLessons(
+    `https://e-spo.ru/org/rasp/export/site/index?pid=1&RaspBaseSearch%5Bgroup_id%5D=${selectedGroup}&RaspBaseSearch%5Bsemestr%5D=osen&RaspBaseSearch%5Bprepod_id%5D=`,
+  );
 
-	if (response && response[number] && typeof response[number] === 'object' && 'lessons' in response[number]) {
-		const lessons = response[number].lessons
-			.map((lesson, index) => {
-				if (!lesson.teacher) {
-					return `#${index + 1} \n----------------------------------------`;
-				}
-				return `#${index + 1}\n${lesson.time}\nКабинет: ${lesson.room}\n${lesson.teacher}\n${lesson.discipline}`;
-			})
-			.join('\n\n');
+  if (response && response[number] && typeof response[number] === 'object' && 'lessons' in response[number]) {
+    const lessons = response[number].lessons
+      .map((lesson, index) => {
+        if (!lesson.teacher) {
+          return `#${index + 1} \n----------------------------------------`;
+        }
+        return `#${index + 1}\n${lesson.time}\nКабинет: ${lesson.room}\n${lesson.teacher}\n${lesson.discipline}`;
+      })
+      .join('\n\n');
 
-		await bot.sendMessage(chatId, lessons);
-	} else {
-		await bot.sendMessage(chatId, 'Нет данных для выбранного дня');
-	}
+    await bot.sendMessage(chatId, lessons);
+  } else {
+    await bot.sendMessage(chatId, 'Нет данных для выбранного дня');
+  }
 });
 
 bot.setMyCommands(commands);
